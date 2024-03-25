c7f981b6ed15a132228742afb43f149a7cbad6f3
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-03-25 16:08:50 +0300

    fix: cover url

--- src/components/BubblePlayer/BubbleTrackInfo.tsx
+++ src/components/BubblePlayer/BubbleTrackInfo.tsx
@@ -35,7 +35,7 @@ const BubbleTrackInfo = (props: BubbleTrackInfoProps) => {
         <div className={`${props.class}  h-[74px] pt-2 bg-black bg-opacity-10 rounded-xl items-center flex flex-col overflow-hidden relative`}>
             <div class='w-full flex justify-between pb-[5px] px-1.5'>
                 <div className="justify-start items-center gap-3.5 flex">
-                    <img className="w-14 h-14 rounded-lg" src={api.forTrackCover(track.musicAlbum.uuid)} />
+                    <img className="w-14 h-14 rounded-lg" src={api.urlForTrackCoverById({id: track.musicAlbum.uuid})} />
                     <div className="flex-col justify-center items-start gap-1 inline-flex">
                         <div className="text-center text-white text-lg font-normal font-['Helvetica Neue'] text-nowrap track-title">{track.title.length > 18 ? track.title.substring(0, 18) + '...' : track.title}</div>
                         <div className="text-center text-white text-opacity-40 text-base font-normal font-['Helvetica Neue']">{track.musicBand.name}</div>
--- src/components/common/AlbumCard.tsx
+++ src/components/common/AlbumCard.tsx
@@ -11,7 +11,7 @@ const AlbumCard = (props: AlbumCardProps) => {
 
     return ( 
         <div onClick={() => nav({to: '/library/albums/$albumId', params: {albumId: props.album.uuid}})} class="flex flex-col cursor-pointer">
-            <img class="w-52 h-52 rounded-md overflow-hidden bg-slate-200 bg-opacity-15" src={api.forTrackCover(props.album.uuid)} alt="" />
+            <img class="w-52 h-52 rounded-md overflow-hidden bg-slate-200 bg-opacity-15" src={api.urlForTrackCoverById({id: props.album.uuid})} alt="" />
             <div class="text-start text-white text-base font-normal capitalize">{props.album.name.substring(0, 25)}</div>
             <div class="text-start text-white text-opacity-60 text-sm font-normal capitalize">{(props.album as any).musicBand?.name.substring(0, 25) ?? 'No name'}</div>
         </div>
