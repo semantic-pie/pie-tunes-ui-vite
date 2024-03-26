832ce8ac6b87a7a43169669dcd36205476365ef1
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-03-26 20:17:27 +0300

    feat: progress bar dragging

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "preact/hooks"
+import { useRef, useState } from "preact/hooks"
 
 type ProgresBarProps = {
     classes?: string
@@ -6,26 +6,45 @@ type ProgresBarProps = {
     relativeValue: number // max value
     value: number // should be between 0 and 100 (percents)
     setValue: (value: number) => void
-    polzunok?: boolean 
+    polzunok?: boolean
 }
 
 const ProgresBar = ({ value, setValue, relativeValue, classes, classesInner, polzunok }: ProgresBarProps) => {
     const container = useRef<HTMLDivElement>(null)
 
+    const [mouseDown, setMouseDown] = useState(false)
+
     const onClickChangeValue = (e: MouseEvent) => {
         if (e.target && container.current) {
-            const { width, left } = container.current.getBoundingClientRect()
-            const x = e.clientX - left
-            setValue(x / width * relativeValue)
+            calculateAndSetProgres(e)
         }
     }
 
+    const calculateAndSetProgres = (e: MouseEvent) => {
+            const { width, left } = container.current.getBoundingClientRect()
+            const x = e.clientX - left
+            setValue(x / width * relativeValue)
+    }
+
+    const onMove = (e: MouseEvent) => {
+        if (mouseDown && e.target && container.current) {
+            calculateAndSetProgres(e)
+        } 
+    }
+
     return (
-        <div ref={container} class={`${classes} h-[4px] cursor-pointer bg-black bg-opacity-20 flex justify-start `} onClick={onClickChangeValue} >
+        <div ref={container}
+            class={`${classes} h-[4px] cursor-pointer bg-black bg-opacity-20 flex justify-start `}
+            onClick={onClickChangeValue} 
+            onMouseDown={() => setMouseDown(true)}
+            onMouseUp={() => setMouseDown(false)}
+            onMouseMove={onMove}
+            onMouseLeave={() => setMouseDown(false)}
+            >
             <div style={{ width: value + '%' }} class={`${classesInner} bg-white bg-opacity-60 cursor-pointer`}>
-            
+
             </div>
-            {polzunok && <div  class={` h-2 w-2 rounded-full bg-white mt-[-2px] ml-[-2px]`}></div>}
+            {polzunok && <div class={` h-2 w-2 rounded-full bg-white mt-[-2px] ml-[-2px] hover:scale-125`}></div>}
         </div>
     )
 }
