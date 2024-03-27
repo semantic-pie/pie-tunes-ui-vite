7b79021b6995b619b9a50ceb8ee4c3b2a4b6d99b
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-03-27 13:08:50 +0300

    feat: progress bar dragging (touch screen)

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -12,35 +12,50 @@ type ProgresBarProps = {
 const ProgresBar = ({ value, setValue, relativeValue, classes, classesInner, polzunok }: ProgresBarProps) => {
     const container = useRef<HTMLDivElement>(null)
 
-    const [mouseDown, setMouseDown] = useState(false)
+    const [pressed, setPressed] = useState(false)
 
-    const onClickChangeValue = (e: MouseEvent) => {
-        if (e.target && container.current) {
-            calculateAndSetProgres(e)
+    const calculateAndSetProgres = (clientX: number) => {
+        if (container.current) {
+            const { width, left } = container.current.getBoundingClientRect()
+            const x = clientX - left
+            setValue(x / width * relativeValue)
         }
     }
 
-    const calculateAndSetProgres = (e: MouseEvent) => {
-            const { width, left } = container.current.getBoundingClientRect()
-            const x = e.clientX - left
-            setValue(x / width * relativeValue)
+    const onTouch = (e: TouchEvent) => {
+        calculateAndSetProgres(e.targetTouches[0].clientX)
     }
 
-    const onMove = (e: MouseEvent) => {
-        if (mouseDown && e.target && container.current) {
-            calculateAndSetProgres(e)
-        } 
+    const onMouse = (e: MouseEvent) => {
+        calculateAndSetProgres(e.clientX)
+    }
+
+
+    const onTouchMove = (e: TouchEvent) => {
+        if (pressed) {
+            calculateAndSetProgres(e.targetTouches[0].clientX)
+        }
+    }
+
+    const onMouseMove = (e: MouseEvent) => {
+        if (pressed) {
+            calculateAndSetProgres(e.clientX)
+        }
     }
 
     return (
         <div ref={container}
             class={`${classes} h-[4px] cursor-pointer bg-black bg-opacity-20 flex justify-start `}
-            onClick={onClickChangeValue} 
-            onMouseDown={() => setMouseDown(true)}
-            onMouseUp={() => setMouseDown(false)}
-            onMouseMove={onMove}
-            onMouseLeave={() => setMouseDown(false)}
-            >
+            onClick={onMouse}
+            onTouchStart={(e) => {setPressed(true), onTouch(e)}}
+            onTouchEnd={() => setPressed(false)}
+            onTouchCancel={() => setPressed(false)}
+            onTouchMove={onTouchMove}
+            onMouseDown={() => setPressed(true)}
+            onMouseUp={() => setPressed(false)}
+            onMouseMove={onMouseMove}
+            onMouseLeave={() => setPressed(false)}
+        >
             <div style={{ width: value + '%' }} class={`${classesInner} bg-white bg-opacity-60 cursor-pointer`}>
 
             </div>
