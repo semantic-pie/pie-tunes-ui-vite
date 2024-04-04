2aa65f12fbf6c2064388ca76cd74f2225cf8c3ff
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-05 00:31:42 +0300

    fix: something went wrong

--- src/components/common/PlaylistCard.tsx
+++ src/components/common/PlaylistCard.tsx
@@ -9,7 +9,7 @@ type PlaylistCardProps = {
 const PlaylistCard = (props: PlaylistCardProps) => {
     return (
         <div onClick={props.onClick} class="w-full flex flex-shrink-0 gap-3 sm:flex-col cursor-pointer">
-            <img class="w-20 h-20 sm:w-full sm:h-full rounded-md overflow-hidden" src={api.urlForTrackCoverById({ id: 'dailyplaylist.jepg' })} alt={props.playlist.name} />
+            <img class="w-20 h-20 sm:w-full sm:h-full rounded-md overflow-hidden" src={api.urlForTrackCoverById({ id: 'dailyplaylist.jpeg' })} alt={props.playlist.name} />
             <div class="sm:w-full">
                 <div class="text-start text-white text-base font-normal capitalize truncate ...">{trancate(props.playlist.name, 28)}</div>
             </div>
