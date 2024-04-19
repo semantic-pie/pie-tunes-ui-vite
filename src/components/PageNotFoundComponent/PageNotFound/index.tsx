2fd3f7c2f9ab5d21025fe8db44d0eb5d4ff401cf
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-19 16:09:37 +0300

    styles: 404 page bg-color

--- src/components/PageNotFoundComponent/PageNotFound/index.tsx
+++ src/components/PageNotFoundComponent/PageNotFound/index.tsx
@@ -6,7 +6,7 @@ export type PageNotFoundProps = {
 
 export const PageNotFound: FunctionalComponent<PageNotFoundProps> = ({ onGoHomeClick }) => {
     return (
-        <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] z-10">
+        <main class="h-screen w-full flex flex-col justify-center items-center z-10">
             <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
             <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
                 Page Not Found
